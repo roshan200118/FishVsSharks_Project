6cffb4f29af6ff37daa83761086671948e187c6a
Roshan Nanthapalan <rnanthap@uwo.ca>
2020-10-11 16:03:53 -0400

    Initial commit

---
+++ FishvsSharks/FishvsSharks/FlopFish.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FishvsSharks
+{
+    class FlopFish : Fish
+    {
+        public const string UNIT_TYPE = "Flop fish";
+        private const int CURRENT_HEALTH = 2;
+        private const int MAXIMUM_HEALTH = 2;
+        private const int REQUIRED_WATERPOWER = 2;
+
+        public FlopFish() : base (UNIT_TYPE, CURRENT_HEALTH, MAXIMUM_HEALTH, REQUIRED_WATERPOWER)
+        {
+            fishImage = Properties.Resources.flopfish;
+            
+        }
+
+       
+        public override Fish CreateFish()
+        {
+            return new FlopFish();
+        }
+    }
+}
